8b0610c7e722290ed01664bd79ee7aeb66e5e934
Albert Su <alcheme@berkeley.edu>
2020-06-13 18:34:59 -0700

    got the bot messaging

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const PORT = process.env.PORT || 8445;
 const WIT_TOKEN = 'ZAIMSMIZL65IR5XEGC7QENDCPDFRTAVV';
 
 // Messenger API parameters
-const FB_PAGE_TOKEN = 'EAAkbrgV33TIBAM5kxZBduQiBw3ZCA7W2JxMzWDALCxD2DxC29Bgrt5f3Xj8UR2gleZBMF3qDy8FCtoTPseyvgdexCU3wpXhPZBjG9XyObcdFbGZCM1l5jK6bAtWPxnKOVszWys356Ts8NEiZCKtDZCOrYZCi0TC0xlWZArZAa3e65tdAZDZD090090cf1708e5a62bb81e01e9e9';
+const FB_PAGE_TOKEN = 'EAAkbrgV33TIBABzpqJ0ildsYw2ziZAuQWR0A9UQVYsBKB18YgMEnCWFw5qFqK9WIaplA5F6QcsSqi9SKWzZBbMyfy3zBemveWgPZBPj6zvjaFMuFJquQ1OZCZAmpikkHO4HjPGWF1iCBKusd7ZAU6YF1oJyHx9cisr6vykE7IVkAZDZD';
 if (!FB_PAGE_TOKEN) { throw new Error('missing FB_PAGE_TOKEN') }
 const FB_APP_SECRET = '298f090090cf1708e5a62bb81e01e9e9';
 if (!FB_APP_SECRET) { throw new Error('missing FB_APP_SECRET') }
@@ -120,9 +120,9 @@ app.use(({method, url}, rsp, next) => {
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 // Webhook setup
-app.get('/webhook', (req, res) => {
+app.get('/', (req, res) => {
   if (req.query['hub.mode'] === 'subscribe' &&
-    req.query['hub.verify_token'] === FB_VERIFY_TOKEN) {
+    req.query['hub.verify_token'] === 'coronacare') {
     res.send(req.query['hub.challenge']);
   } else {
     res.sendStatus(400);
@@ -130,7 +130,7 @@ app.get('/webhook', (req, res) => {
 });
 
 // Message handler
-app.post('/webhook', (req, res) => {
+app.post('/', (req, res) => {
   // Parse the Messenger payload
   // See the Webhook reference
   // https://developers.facebook.com/docs/messenger-platform/webhook-reference
@@ -164,6 +164,8 @@ app.post('/webhook', (req, res) => {
               console.log(intents);
               console.log(entities);
               console.log(traits);
+              console.log(text);
+              console.log(sender);
               // For now, let's reply with another automatic message
               fbMessage(sender, `We've received your message: ${text}.`);
             })
@@ -212,4 +214,4 @@ function verifyRequestSignature(req, res, buf) {
 }
 
 app.listen(PORT);
-console.log('Listening on :' + PORT + '...');
+console.log('Listening on :' + PORT + '...')
